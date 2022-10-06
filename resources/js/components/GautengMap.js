ec621a4d16f4e58630bd1fd6bf09d795a7163d1a
katlego <katlego@i-t-e.co.za>
2022-10-06 20:14:33 +0200

    updated Map with clustering

--- resources/js/components/GautengMap.js
+++ resources/js/components/GautengMap.js
@@ -1,23 +1,32 @@
-import React, {useLayoutEffect, useRef} from 'react';
-import {Inertia} from "@inertiajs/inertia";
-import { MapContainer, TileLayer, useMap ,Marker,Popup} from 'react-leaflet'
-import Map from "./Map";
-import {Card, Rate, Space} from 'antd';
+import React from 'react';
+import { MapContainer, TileLayer ,Marker,Popup} from 'react-leaflet'
+import {Card} from 'antd';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import ResetViewControl from '@20tab/react-leaflet-resetview';
 
 
 function GautengMap({gpsdata}) {
     const totalCoords =gpsdata.length;
     const listItems = gpsdata.map((coord,index) =>
-         <Marker key={index} position={[coord.Latitude,coord.Longitude]}>
-            <Popup> interview number: {index+1} of {totalCoords}
+        <Marker key={index} position={[coord.GPS_LA,coord.GPS_LO]}>
+            <Popup> interview number:{coord.SbjNum}
             </Popup>
         </Marker>
     );
+    const createClusterCustomIcon = function (cluster) {
+        return L.divIcon({
+            html: `<span>`+cluster.getChildCount()+`</span>`,
+            className: 'marker-cluster-custom',
+            iconSize: L.point(40, 40, true),
+  });
+};
+
     return (
         <div style={{ height: "60px" }}>
 
             <Card
-                title={'Interview locations'}
+                title={'Interview locations '}
+                extra={ <span style={{color:"white"}}> n = {totalCoords} </span>}
                 headStyle={{
                     backgroundColor:"#d45300",
                     color:"#ffffff"
@@ -28,19 +37,26 @@ function GautengMap({gpsdata}) {
                     borderColor:"#d45300",
                 }}
             >
-            <MapContainer
+                <MapContainer
                 center={[-25.9363439, 28.0813105]}
                 zoom={8.5}
                 scrollWheelZoom={false}
-
+                className="markercluster-map"
             >
+                <ResetViewControl
+                    title="Reset view"
+                    icon="R"
+                />
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-
-                {listItems}
-            </MapContainer>
+                    <MarkerClusterGroup
+                        iconCreateFunction={createClusterCustomIcon}
+                    >
+                        {listItems}
+                    </MarkerClusterGroup>
+             </MapContainer>
             </Card>
         </div>
     );
