9717d73960b6cde1a110c010d2b0111578bef000
katlego <katlego@i-t-e.co.za>
2022-10-06 20:12:00 +0200

    removed unused function

--- app/Http/Controllers/ArchievedController.php
+++ app/Http/Controllers/ArchievedController.php
@@ -14,47 +14,41 @@ class ArchievedController extends Controller
     {
         $dbQuery = Archieved::select(
             DB::raw('COUNT(achieved.id) as total'),
-
             DB::raw('sum( ( CASE WHEN achieved.Sector = 1 THEN 1 ELSE 0 END  ) )  as sum_manufacturing_primary_sector'),
             DB::raw('sum( ( CASE WHEN achieved.Sector = 2 THEN 1 ELSE 0 END  ) )  as sum_manufacturing_secondary_sector '),
-
             DB::raw('sum( ( CASE WHEN achieved.SampleType = 1 THEN 1 ELSE 0 END  ) )  as sum_sample_2019'),
             DB::raw('sum( ( CASE WHEN achieved.SampleType = 2 THEN 1 ELSE 0 END  ) )  as sum_sample_2022 '),
-
             DB::raw('sum( ( CASE WHEN achieved.FirmSize = 1 THEN 1 ELSE 0 END  ) )  as sum_firmsize_A'),
             DB::raw('sum( ( CASE WHEN achieved.FirmSize = 2 THEN 1 ELSE 0 END  ) )  as sum_firmsize_B '),
             DB::raw('sum( ( CASE WHEN achieved.FirmSize = 3 THEN 1 ELSE 0 END  ) )  as sum_firmsize_C '),
-
         );
         $data = $dbQuery->get();
-
-       $manufacturingData =  DB::table('achieved')->select(
-           'Q_H_O1',
-           'Q_H_O2',
-           'Q_H_O3',
-           'Q_H_O4',
-           'Q_H_O5',
-           'Q_H_O6',
-           'Q_H_O7',
-           'Q_H_O8',
-           'Q_H_O9',
-           'Q_H_O10',
-           'Q_H_O11',
-           'Q_H_O12',
-           'Q_H_O13',
-           'Q_H_O14',
-           'Q_H_O15',
-           'Q_H_O16',
-           'Q_H_O17',
-           'Q_H_O18',
-           'Q_H_O19',
-           'Q_H_O20',
-           'Q_H_O21',
-           'Q_H_O22',
-           'Q_H_O23',
-           'Q_H_O24',
-
-       );
+        $manufacturingData =  DB::table('achieved')->select(
+            'Q_H_O1',
+            'Q_H_O2',
+            'Q_H_O3',
+            'Q_H_O4',
+            'Q_H_O5',
+            'Q_H_O6',
+            'Q_H_O7',
+            'Q_H_O8',
+            'Q_H_O9',
+            'Q_H_O10',
+            'Q_H_O11',
+            'Q_H_O12',
+            'Q_H_O13',
+            'Q_H_O14',
+            'Q_H_O15',
+            'Q_H_O16',
+            'Q_H_O17',
+            'Q_H_O18',
+            'Q_H_O19',
+            'Q_H_O20',
+            'Q_H_O21',
+            'Q_H_O22',
+            'Q_H_O23',
+            'Q_H_O24',
+        );
 
         $getData = $manufacturingData->get();
 
@@ -67,30 +61,22 @@ class ArchievedController extends Controller
             'manufacturing_table' => $groupManufacturingData,
 
         ]);
-
-
     }
-
-    public function getPercentage($total, $values)
-    {
-        return ($values);
-    }
-
 
     private function Manufacturing(\Illuminate\Support\Collection $collection):array
     {
         $data = [];
 
         foreach ($collection as $key => $nested) {
-            foreach ( $nested as $values) {
+            foreach ($nested as $values) {
                 switch ($values) {
-                    case 1 :
+                    case 1:
                         $data['manufacturing_of_food'][] = $values;
                         break;
-                    case 2 :
+                    case 2:
                         $data['manufacturing_of_beverage'][] = $values;
                         break;
-                    case 3 :
+                    case 3:
                         $data['manufacturing_of_tobacco'][] = $values;
                         break;
                     case 4:
@@ -156,9 +142,6 @@ class ArchievedController extends Controller
                     case 24:
                         $data['manufacturing_of_Repair'][] = $values;
                         break;
-
-
-
                 }
             }
         }
